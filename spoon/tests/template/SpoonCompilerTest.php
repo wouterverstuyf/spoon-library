c0782538546acbba3fb6c9f7ea4a825df560677d
woutersioen <wouter.sioen@wijs.be>
2014-11-06 12:43:33 +0100

    Add tests for the template parsing.

---
+++ spoon/tests/template/SpoonCompilerTest.php
@@ -0,0 +1,110 @@
+<?php
+
+date_default_timezone_set('Europe/Brussels');
+
+$includePath = dirname(dirname(dirname(dirname(__FILE__))));
+set_include_path(get_include_path() . PATH_SEPARATOR . $includePath);
+
+require_once 'spoon/spoon.php';
+require_once 'PHPUnit/Framework/TestCase.php';
+
+class SpoonTemplateCompilerTest extends PHPUnit_Framework_TestCase
+{
+	function testParseVariables()
+	{
+		// create a spoon template
+		$tpl = new SpoonTemplate();
+		$tpl->setForceCompile(true);
+		$tpl->setCompileDirectory(dirname(__FILE__) . '/cache');
+
+		$tpl->assign('variable', 'value');
+
+		// fetch the content from the template
+		$this->assertEquals(
+			'value',
+			$tpl->getContent(dirname(__FILE__) . '/templates/variable.tpl')
+		);
+	}
+
+	function testParseArrays()
+	{
+		// create a spoon template
+		$tpl = new SpoonTemplate();
+		$tpl->setForceCompile(true);
+		$tpl->setCompileDirectory(dirname(__FILE__) . '/cache');
+
+		$tpl->assign(
+			'array',
+			array('name' => 'Array name')
+		);
+
+		// fetch the content from the template
+		$this->assertEquals(
+			'Array name',
+			$tpl->getContent(dirname(__FILE__) . '/templates/array.tpl')
+		);
+	}
+
+	function testParseObjects()
+	{
+		// create a spoon template
+		$tpl = new SpoonTemplate();
+		$tpl->setForceCompile(true);
+		$tpl->setCompileDirectory(dirname(__FILE__) . '/cache');
+
+		// add an object
+		$object = new Object();
+		$object->setName('Object name');
+		$tpl->assign('object', $object);
+
+		// fetch the content from the template
+		$this->assertEquals(
+			'Object name',
+			$tpl->getContent(dirname(__FILE__) . '/templates/object.tpl')
+		);
+	}
+
+	function testParseNestedArrays()
+	{
+		// create a spoon template
+		$tpl = new SpoonTemplate();
+		$tpl->setForceCompile(true);
+		$tpl->setCompileDirectory(dirname(__FILE__) . '/cache');
+
+		$tpl->assign(
+			'array',
+			array(
+				'inner_array' => array(
+					'name' => 'Array name'
+				)
+			)
+		);
+
+		// fetch the content from the template
+		$this->assertEquals(
+			'Array name',
+			$tpl->getContent(dirname(__FILE__) . '/templates/nested_array.tpl')
+		);
+	}
+}
+
+/**
+ * POPO to test spoon template with
+ */
+class Object
+{
+	protected $name;
+	protected $object;
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function setName($name)
+	{
+		$this->name = $name;
+
+		return $this;
+	}
+}
---
+++ spoon/tests/template/templates/nested_array.tpl
@@ -0,0 +1,1 @@
+{$array.inner_array.name}
