3f06f5f0d8344d2b9d5e4e32b2ce070f62b965ce
Steven Dickinson <github@orangestems.co.uk>
2015-05-18 10:36:32 +0100

    Failing test for integer and float valued radio buttons

--- spoon/tests/form/SpoonFormRadiobuttonTest.php
+++ spoon/tests/form/SpoonFormRadiobuttonTest.php
@@ -17,6 +17,11 @@ class SpoonFormRadiobuttonTest extends PHPUnit_Framework_TestCase
 	 */
 	protected $rbtGender;
 
+	/**
+	 * @var	SpoonFormRadiobutton
+	 */
+	protected $rbtNumeric;
+
 	public function setup()
 	{
 		$this->frm = new SpoonForm('radiobutton');
@@ -24,6 +29,12 @@ class SpoonFormRadiobuttonTest extends PHPUnit_Framework_TestCase
 		$gender[] = array('label' => 'Male', 'value' => 'M');
 		$this->rbtGender = new SpoonFormRadiobutton('gender', $gender, 'M');
 		$this->frm->add($this->rbtGender);
+
+		$numeric = array(
+			array('value' => 1,   'label' => 'One'),
+			array('value' => 1.5, 'label' => 'One And A Half'),
+		);
+		$this->rbtNumeric = new SpoonFormRadiobutton('numeric', $numeric, '1');
 	}
 
 	public function testGetChecked()
@@ -50,5 +61,52 @@ class SpoonFormRadiobuttonTest extends PHPUnit_Framework_TestCase
 		$this->assertFalse($this->rbtGender->isFilled());
 		$_POST['gender'] = array('foo', 'bar');
 		$this->assertFalse($this->rbtGender->isFilled());
+	}
+
+	public function testIntegerValues()
+	{
+		$this->assertEquals('1', $this->rbtNumeric->getValue());
+
+		$buttons = $this->rbtNumeric->parse();
+
+		$expected = $this->getExpectedNumericArray('1');
+
+		$this->assertEquals($buttons, $expected);
+	}
+
+	public function testFloatValues()
+	{
+		$this->rbtNumeric->setChecked(1.5);
+
+		$this->assertEquals('1.5', $this->rbtNumeric->getValue());
+
+		$buttons = $this->rbtNumeric->parse();
+
+		$expected = $this->getExpectedNumericArray('1.5');
+
+		$this->assertEquals($buttons, $expected);
+	}
+
+	protected function getExpectedNumericArray($checked)
+	{
+		$oneChecked = ($checked === '1') ? ' checked="checked"' : '';
+		$oneHalfChecked = ($checked === '1.5') ? ' checked="checked"' : '';
+
+		return array(
+			array(
+				'rbtNumeric' => '<input type="radio" name="numeric" value="1"' . $oneChecked . ' class="inputRadiobutton" id="numeric1" />',
+				'id' => 'numeric1',
+				'label' => 'One',
+				'value' => '1',
+				'element' => '<input type="radio" name="numeric" value="1"' . $oneChecked . ' class="inputRadiobutton" id="numeric1" />',
+			),
+			array(
+				'rbtNumeric' => '<input type="radio" name="numeric" value="1.5"' . $oneHalfChecked . ' class="inputRadiobutton" id="numeric1.5" />',
+				'id' => 'numeric1.5',
+				'label' => 'One And A Half',
+				'value' => '1.5',
+				'element' => '<input type="radio" name="numeric" value="1.5"' . $oneHalfChecked . ' class="inputRadiobutton" id="numeric1.5" />',
+			)
+		);
 	}
 }
