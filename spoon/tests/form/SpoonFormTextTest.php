1e1b0909f26863f8f11fa1a0bbbcb896f343e25f
woutersioen <wouter.sioen@gmail.com>
2016-04-13 15:58:27 +0200

    Add a shorthand to make a form field required.
    This adds the html5 required attribute to our field causing html5
    validation to be run on it.

--- spoon/tests/form/SpoonFormTextTest.php
+++ spoon/tests/form/SpoonFormTextTest.php
@@ -301,4 +301,34 @@ class SpoonFormTextTest extends PHPUnit_Framework_TestCase
 			$this->txtName->getAttributes()['type']
 		);
 	}
+
+	public function testRequired()
+	{
+		$this->txtName->makeRequired();
+		$this->assertEquals(
+			'required',
+			$this->txtName->getAttributes()['required']
+		);
+	}
+
+	public function testChainingMethods()
+	{
+		$this->txtName
+			->setType('email')
+			->makeRequired()
+			->setError('test');
+
+		$this->assertEquals(
+			'email',
+			$this->txtName->getAttributes()['type']
+		);
+		$this->assertEquals(
+			'required',
+			$this->txtName->getAttributes()['required']
+		);
+		$this->assertEquals(
+			'test',
+			$this->txtName->getErrors()
+		);
+	}
 }
