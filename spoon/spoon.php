6f208f2b6d35eff0c7ab362d323bbaca2004fda3
Jelmer Prins <jelmer@sumocoders.be>
2017-08-02 11:37:53 +0200

    Clean up the autoloader of spoon

--- spoon/spoon.php
+++ spoon/spoon.php
@@ -112,13 +112,9 @@ class Spoon
 		$classes['spoondatagridsourcedb'] = 'datagrid/source_db.php';
 		$classes['spoondate'] = 'date/date.php';
 		$classes['spoondirectory'] = 'directory/directory.php';
-		$classes['spoonemail'] = 'email/email.php';
-		$classes['spoonemailsmtp'] = 'email/smtp.php';
 		$classes['spoonfeedexception'] = 'feed/exception.php';
 		$classes['spoonfeedrss'] = 'feed/rss.php';
 		$classes['spoonfeedrssitem'] = 'feed/rss_item.php';
-		$classes['spoonfeedatomrss'] = 'feed/atom_rss.php';
-		$classes['spoonfeedatomrssitem'] = 'feed/atom_rss_item.php';
 		$classes['spoonfile'] = 'file/file.php';
 		$classes['spoonfilecsv'] = 'file/csv.php';
 		$classes['spoonfilter'] = 'filter/filter.php';
@@ -140,18 +136,11 @@ class Spoon
 		$classes['spoonformtextarea'] = 'form/textarea.php';
 		$classes['spoonformtime'] = 'form/time.php';
 		$classes['spoonhttp'] = 'http/http.php';
-		$classes['spoonicalexception'] = 'ical/exception.php';
-		$classes['spoonical'] = 'ical/ical.php';
-		$classes['spoonicalitem'] = 'ical/ical';
-		$classes['Spoonicalevent'] = 'ical/ical';
 		$classes['spoonlocale'] = 'locale/locale.php';
-		$classes['spoonlog'] = 'log/log.php';
-		$classes['spoonrestclient'] = 'rest/client.php';
 		$classes['spoontemplate'] = 'template/template.php';
 		$classes['spoontemplatecompiler'] = 'template/compiler.php';
 		$classes['spoontemplatemodifiers'] = 'template/modifiers.php';
 		$classes['spoonthumbnail'] = 'thumbnail/thumbnail.php';
-		$classes['spoonxmlrpcclient'] = 'xmlrpc/client.php';
 
 		// path
 		$path = dirname(realpath(__FILE__));
