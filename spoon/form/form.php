fca1103ad2d8150f0a7447048ca6e6d117c4a27b
Tijs Verkoyen <tijs@verkoyen.eu>
2020-12-16 16:34:53 +0100

    Use our previously stored variable
    If we use `$this->getField('form_token')->getValue();` it will use the field
    that is automagically added, instead of the real submitted value.

--- spoon/form/form.php
+++ spoon/form/form.php
@@ -1162,10 +1162,6 @@ class SpoonForm
 			// token was found
 			else
 			{
-				// get the submitted token
-				$submittedToken = $this->getField('form_token')->getValue();
-
-				// compare tokens
 				if($submittedToken != $this->getTokenFromSession()) $errors[] = $this->tokenError;
 			}
 		}
